b32e97857db6966b0f128aba34764ee73c836d3b
Annica Burns <annica.burns@healthcatalyst.com>
2015-11-10 15:11:42 -0500

    Initial Commit

---
+++ SiestaRealmIntegration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SiestaRealmIntegration
+//
+//  Created by Annica Burns on 11/10/15.
+//  Copyright © 2015 Health Catalyst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
